3968cce6aeafe81f0ef8cbf4b491d2ac89e3e41b
Armando-Jr <54594382+Armando-Jr@users.noreply.github.com>
2019-11-30 17:22:55 -0600

    Función lambda

---
+++ lambda.py
@@ -0,0 +1,27 @@
+# FUNCIONES ANÓNIMAS
+# Las funciones normales son definiodas con la palabra
+# clave def, mientras que las anónimas se crean con lambda
+# lambda x : x + 1  --> x(argumento): x + 1 (cuerpo de la función)
+
+lista = [1,2,3,4,5]
+
+# se crea una nueva lista usando map para elevar
+# cada elemento de lista al cuadrado
+
+lista_m = list(map(lambda x:x**2,lista))
+print(lista_m) #imprime 1,4,9,16,25
+
+# Asignación de función lambda a una variable
+mayores_a_2 = lambda x:x>2
+# Uso de la función filtro con la función lambda
+# solo filtará elementos mayores a 2
+lista_f1 = list(map(mayores_a_2,lista))
+print(lista_f1) # imprime falsey true
+
+lista_f2 = list(filter(mayores_a_2,lista))
+print(lista_f2) # imprimira 3 ,4 ,5
+
+# Variable con función lambda para elevar a la 5
+
+potencia = lambda x:x**5
+print(potencia(3))#imprimira 243
